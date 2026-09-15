01e7910ccef08eebd7dbf9576c44e541db04e708
Elango <Elangoofficial49@gmail.com>
2026-09-15 20:08:01 +0530

    style: reformat ai_assistant_dialog.dart after null-check fix

--- mobile/flutter_app/lib/widgets/ai_assistant_dialog.dart
+++ mobile/flutter_app/lib/widgets/ai_assistant_dialog.dart
@@ -39,7 +39,7 @@ class _AiAssistantDialogState extends State<AiAssistantDialog> {
         'lang': langCode,
       });
 
-      if (res['success'] == true) { 
+      if (res['success'] == true) {
         setState(() {
           _messages.add({
             "role": "assistant",
